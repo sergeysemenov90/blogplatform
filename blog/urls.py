f08803faaa797d66a97a34076c4aba25227b6e79
sergeysemenov90 <semenovsg90@gmail.com>
2021-12-13 14:39:25 +0300

    add html-file for PostDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import PostListView, PostDetailView
 
@@ -6,3 +8,6 @@ urlpatterns = [
     path('posts', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogplatform/settings.py
+++ blogplatform/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -128,7 +127,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
