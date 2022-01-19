b03d4941a6a62fd3f4ae9ba545089a9de7c92f2e
Sergey Semenov <semenovsg90@gmail.com>
2022-01-19 13:58:35 +0300

    add domain to allowed hosts

--- blogplatform/settings.py
+++ blogplatform/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '64.227.39.184']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '64.227.39.184', 'blogplatform.ru']
 
 # Application definition
 
